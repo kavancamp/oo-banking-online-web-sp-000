d99acc63e3f642229719277b9206e70e7c26a3bc
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-05 22:41:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   def execute_transaction
    if self.status == "complete"
      return
-   elsif self.amount > self.sender.balance
+   elsif self.amount >= self.sender.balance
      self.status = "rejected"
      return "Transaction rejected. Please check your account balance."
    else
